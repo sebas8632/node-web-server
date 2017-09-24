13b72d58694fa163d730f2a803a1308da81753a8
Juan Sebastián Flórez Saavedra <sebastian8632@gmail.com>
2017-09-24 16:31:39 -0500

    Initial Commit

---
+++ server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear();
+});
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+app.set('view engine', 'hbs');
+
+
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} --- ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err){
+      console.log(err);
+    }
+  });
+  next();
+});
+
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+//   //next();
+// });
+
+
+
+app.get('/', (req, res) => {
+  res.render('home.hbs', {
+    pageTitle : 'Home Page',
+    welcomeMessage : 'Welcome to my website.',
+    currentYear : new Date().getFullYear()
+  });
+});
+
+app.get('/about', (req, res) => {
+res.render('about.hbs', {
+  pageTitle : 'About Page',
+  currentYear : new Date().getFullYear()
+});
+});
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage : 'Unable to handled request'
+  });
+});
+app.listen(3000, () => {
+  console.log('Server is up in port 3000.');
+});
